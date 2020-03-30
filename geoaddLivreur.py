c345377814c76de867666811c998c92cbf2c0dbb
LaCharogne <58693493+LaCharogne@users.noreply.github.com>
2020-03-30 17:31:19 +0200

    Update geoaddLivreur.py

--- geoaddLivreur.py
+++ geoaddLivreur.py
@@ -11,7 +11,7 @@ redis_password = ""
 def add_Delivery_Boy() :
     try:
         r = redis.Redis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
-        r.geoadd('StoreAppLivraison',4.861934,45.766797,'Livreur:12')
+        r.geoadd('StoreAppLivraison',4.861934,45.766797,'Livreur:6')
         print('Livreur ajouté!')
         print('Distance du Livreur 6 par rapport au Metro Gratte-Ciel :')
         print(r.geodist('StoreAppLivraison','Lieu:Metro Gratte-Ciel','Livreur:6'))
